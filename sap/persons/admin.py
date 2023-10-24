0ed442ce8a819dadcd22e4435c1bc2b9b5928265
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-24 18:06:31 -0400

    adding app to admin site

--- sap/persons/admin.py
+++ sap/persons/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from persons.models import Person
+from persons.models import Home, Person
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Home)
